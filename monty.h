0a687c6bc8198888c22fd556075ad0ecc4700abc
la victoire <lavictoire633@gmail.com>
2023-06-21 12:06:54 -0700

    commiting my changes to monty.h before I git pull to get your files

--- monty.h
+++ monty.h
@@ -32,10 +32,32 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* declaring the global variables */
+/**
+ * struct var - the global variables for this project
+ * @args: an array to store the commands
+ * @file: the pointer to the monty file
+ * @top: to keep count of the number of items in the stack or queue
+ *
+ * Description: the global variables needed for this project
+ * for use in the main program
+ */
+
+typedef struct var
+{
+	char *args;
+	FILE *file;
+	int top;
+} var_t
+
+/* declaring the external variable for sharing in other source code files */
+extern var_t var_s;
+
 #include <stdio.h>
 #include <stdlib.h>
 
 stack_t *add_node(stack_t **head, int n);
 FILE *read_file(int argc, char *argv[]);
+void push(stack_t **head, unsigned int linenum);
 
 #endif /* MONTY_H */
