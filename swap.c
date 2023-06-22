02e422b92c7f1078762603020860a3e38a971c48
LynneDC <roselinedc1308@gmail.com>
2023-06-22 16:52:34 +0200

    daoen

---
+++ swap.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to the stack
+ * @line_number: current line number
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	
+	int value;
+	stack_t *temp = *stack;
+	if (!stack)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = temp->n;
+	temp = temp->next;
+	temp->next->n = value;
+}
