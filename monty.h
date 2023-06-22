f47813e73e9c14b12c8f1c2ee2d4656e63f6add0
LynneDC <roselinedc1308@gmail.com>
2023-06-22 18:59:43 +0200

    update

--- monty.h
+++ monty.h
@@ -1,5 +1,8 @@
 #ifndef MONTY_H
 #define MONTY_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -17,8 +20,17 @@ typedef struct stack_s
         struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
+
+/*prototypes*/
+void pall_stack(const stack_t *h, int count);
+stack_t *push(stack_t **head,  const int n);
+void pop(stack_t **head, unsigned int top);
+stack_t *pint(stack_t **head, unsigned int idx);
+void swap(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void execute_inst(char *str, int line_num);
+
+ /**
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
@@ -51,8 +63,9 @@ extern var_t var_s;
 #include <stdio.h>
 #include <stdlib.h>
 
+void execute_command(char *command, char *arg, int line_number);
 FILE *read_file(int argc, char *argv[]);
-void push_int(stack_t **head, unsigned int linenum);
+void push_int(stack_t **head, unsigned int linenum, int i);
 stack_t *add_node(stack_t **head, int n);
 
 #endif /* MONTY_H */
